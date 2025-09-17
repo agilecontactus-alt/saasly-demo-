c9ac49acd96f975c0565fddf5a7dde55cb729676
agilecontactus-alt <agilecontactus@gmail.com>
2025-09-17 20:40:17 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,14 @@
-print('Hello from SaaSly Backend;)
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to SaaSly Demo 🚀"}
+
+@app.get("/users")
+def get_users():
+    return [
+        {"id": 2, "name": "Krishna", "role": "Scrum Master"},
+        {"id": 3, "name": "Harini", "role": "Developer"},
+    ]
